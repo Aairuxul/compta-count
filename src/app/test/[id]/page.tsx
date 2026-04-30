ccb4020bc60c229dfce292a987c5c8f7a612a6b0
ValentinRusseil <valentinrusseil52@gmail.com>
2026-04-30 13:27:29 +0200

    fix: handle missing test ID and update display message

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import NotFound from '@/app/not-found';
 import { useParams } from 'next/navigation';
 
 export default function TestId() {
     const params = useParams();
     const id = params.id;
 
+    if (!id) {
+      NotFound();
+    }
+
     return (
       <div className="flex min-h-screen flex-col flex-1 items-center justify-center bg-transparent font-sans text-[var(--foreground)]">
         <div className="flex flex-col flex-1 items-center justify-center">
-          Affiche l'id de la page : {id}
+          Affiche l'id test de la page : {id}
         </div>
       </div>
     );
