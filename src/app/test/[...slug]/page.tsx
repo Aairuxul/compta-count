752e6c678ff39e2a14fa983c80488d6fc716b55b
ValentinRusseil <valentinrusseil52@gmail.com>
2026-04-30 13:43:33 +0200

    add: create TestId page to display test ID from URL slug

--- src/app/test/[...slug]/page.tsx
+++ src/app/test/[...slug]/page.tsx
@@ -5,16 +5,20 @@ import { useParams } from 'next/navigation';
 
 export default function TestId() {
     const params = useParams();
-    const id = params.id;
+    const slug = params.slug as string[];
 
-    if (!id) {
-      NotFound();
+    if (!slug || !Array.isArray(slug) || slug.length === 0) {
+      return NotFound();
     }
 
     return (
       <div className="flex min-h-screen flex-col flex-1 items-center justify-center bg-transparent font-sans text-[var(--foreground)]">
         <div className="flex flex-col flex-1 items-center justify-center">
-          Affiche l'id test de la page : {id}
+          <p>Affiche l'id test de la page : {slug[slug.length - 1]}</p>
+          <pre className="mt-4 p-4 bg-gray-100 rounded text-sm text-black">
+            Toute la partie slug :
+            {JSON.stringify(slug, null, 2)}
+          </pre>
         </div>
       </div>
     );
