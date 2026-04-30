9d62248b161613c98f63b2e45b9b29e1396092c9
Sins-Luxuria <pmatteo032002@gmail.com>
2026-04-30 11:53:34 +0200

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        ${menuLayout({ children })}
+        {menuLayout({ children })}
       </body>
     </html>
   );
